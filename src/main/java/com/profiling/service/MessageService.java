0932569bd9240d35476e6355561a42167ff29c00
jarekczaplicki <jarekczaplicki@tlen.pl>
2024-11-17 08:42:08 +0100

    using of table of objects

--- src/main/java/com/profiling/service/MessageService.java
+++ src/main/java/com/profiling/service/MessageService.java
@@ -13,10 +13,27 @@ public class MessageService {
         this.messageSource = messageSource;
         String textEn = messageSource.getMessage(
                 "hello"
-                ,new Object[]{}
+                // indexes       0           1
+                ,new Object[]{"Jarek", "Czaplicki"} // 2
                 , Locale.ENGLISH
 
         );
         System.out.println(textEn);
+
+        String textPL = messageSource.getMessage(
+                "hello"
+                ,new Object[]{"Chlebek", "", "", "", "", "", ""}
+                , Locale.forLanguageTag("pl")
+
+        );
+        System.out.println(textPL);
+Object[] objects = {};
+        String textEE = messageSource.getMessage(
+                "hello"
+                ,new Object[]{"Kristina Lokko", "ja", "Raimond Olle"}
+                , Locale.forLanguageTag("ee")
+
+        );
+        System.out.println(textEE);
     }
 }
